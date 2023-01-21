262c1bf8a713ab1950ad58f2e13ceca1cf9270a5
임성민 <hcooch2ch3@gmail.com>
2023-01-21 23:12:51 +0900

    Time: 2 ms (68.03%), Space: 14.4 MB (72.40%) - LeetHub

---
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.swift
@@ -0,0 +1,31 @@
+
+class MyQueue {
+    var queue: [Int] = []
+
+    init() {}
+    
+    func push(_ x: Int) {
+        queue.append(x)
+    }
+    
+    func pop() -> Int {
+        return queue.removeFirst()
+    }
+    
+    func peek() -> Int {
+        return queue.first!
+    }
+    
+    func empty() -> Bool {
+        return queue.count == 0
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * let obj = MyQueue()
+ * obj.push(x)
+ * let ret_2: Int = obj.pop()
+ * let ret_3: Int = obj.peek()
+ * let ret_4: Bool = obj.empty()
+ */
