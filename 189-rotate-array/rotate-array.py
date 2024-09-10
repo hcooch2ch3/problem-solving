0eedd1bbbfda1ab28c2165b24bf14f51ac8b9eaa
임성민 <hcooch2ch3@gmail.com>
2024-09-11 01:51:34 +0900

    Time: 1117 ms (10.05%) | Memory: 27.9 MB (66.58%) - LeetSync

---
+++ 189-rotate-array/rotate-array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        for _ in range(k):
+            num = nums.pop()
+            nums.insert(0, num)
+        
+        
+        
